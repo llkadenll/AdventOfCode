86cd8f5606f580be38c1fac240876309c238007c
llkadenll <kdwornik@o2.pl>
2022-09-12 22:41:31 +0200

    Day 2 solved

---
+++ Day 2/day2b.rb
@@ -0,0 +1,38 @@
+def read_file(file_name)
+  plan = Array.new()
+  file = File.open(file_name)
+  file_data = file.readlines.map(&:chomp)
+
+  for i in 0...file_data.length do
+    plan.push(file_data[i])
+  end
+
+  plan
+end
+
+def calculate_position(plan)
+  horizontal = 0
+  depth = 0
+  aim = 0
+
+  plan.each do |arr|
+    move = arr.split()
+    case move[0]
+    when "up"
+      aim -= move[1].to_i
+    when "down"
+      aim += move[1].to_i
+    when "forward"
+      horizontal += move[1].to_i
+      depth += aim * move[1].to_i
+    end
+
+    puts "#{horizontal} #{depth} #{aim}"
+  end
+
+  total = horizontal * depth
+  total
+end
+
+plan = read_file("day2.txt")
+puts calculate_position(plan)
